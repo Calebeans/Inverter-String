3198e36f60118e02263176cb2779dd75c2f0c61d
Calebe Ribeiro <rcalebe655@gmail.com>
2024-03-06 16:14:30 -0300

    Invertendo-string

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+
+    cout << "Digite uma string: ";
+    getline(cin, str);
+
+    int n = str.length();
+
+    for (int i = 0; i < n / 2; i++) {
+        swap(str[i], str[n - i - 1]);
+    }
+
+    cout << "String invertida: " << str << endl;
+
+    return 0;
+}
